3456d026b93bb9c836817fea85cce830b5cf5d81
Nishi <nishi@nishi.boats>
2024-11-27 20:42:10 +0000

    widgets
    git-svn-id: file:///raid/svn-personal/harp/trunk@11 86f897ea-aefc-b749-bab6-ff70750bda52

--- Browser/gui_motif.c
+++ Browser/gui_motif.c
@@ -1,1 +1,32 @@
 /* $Id$ */
+
+#include <stdio.h>
+
+#include <X11/X.h>
+#include <X11/Intrinsic.h>
+#include <Xm/Xm.h>
+
+Widget top;
+XtAppContext ctx;
+extern int argc;
+extern char** argv;
+
+int harp_gui_init(void) {
+	XInitThreads();
+	top = XtVaAppInitialize(&ctx, "Harp", NULL, 0, &argc, argv, NULL, XtNtitle, "Harp", NULL);
+	if(top == NULL) {
+		fprintf(stderr, "Failed to create the main window\n");
+		return 1;
+	}
+	XtRealizeWidget(top);
+	return 0;
+}
+
+int harp_gui_loop(void) {
+	XEvent ev;
+	while(1) {
+		XtAppNextEvent(ctx, &ev);
+		XtDispatchEvent(&ev);
+	}
+	return 0;
+}
--- Browser/main.c
+++ Browser/main.c
@@ -4,8 +4,17 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv) {
+int harp_gui_init(void);
+int harp_gui_loop(void);
+
+int argc;
+char** argv;
+
+int main(int _argc, char** _argv) {
 	int i = 0;
+	int st;
+	argc = _argc;
+	argv = _argv;
 	for(i = 1; i < argc; i++) {
 		if(argv[i][0] == '-') {
 			if(strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
@@ -20,4 +29,6 @@ int main(int argc, char** argv) {
 			/* URL */
 		}
 	}
+	if((st = harp_gui_init())) return st;
+	return harp_gui_loop();
 }
