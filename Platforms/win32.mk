714ec376a97bfce0a37a9b44cbb024d667580992
Nishi <nishi@nishi.boats>
2024-11-28 22:09:38 +0000

    add windows
    git-svn-id: file:///raid/svn-personal/harp/trunk@17 86f897ea-aefc-b749-bab6-ff70750bda52

---
+++ Platforms/win32.mk
@@ -0,0 +1,8 @@
+# $Id$
+
+include $(TOPDIR)/Platforms/generic.mk
+
+GUI = gdi
+
+AR = i686-w64-migw32-gcc
+CC = i686-w64-mingw32-gcc
---
+++ Platforms/win64.mk
@@ -0,0 +1,8 @@
+# $Id$
+
+include $(TOPDIR)/Platforms/generic.mk
+
+GUI = gdi
+
+AR = x86_64-w64-migw32-gcc
+CC = x86_64-w64-mingw32-gcc
