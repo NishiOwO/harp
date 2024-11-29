9167dced877c41241ff9506e3f5e141c9e49717e
Nishi <nishi@nishi.boats>
2024-11-29 18:07:40 +0000

    adding info
    git-svn-id: file:///raid/svn-personal/harp/trunk@31 86f897ea-aefc-b749-bab6-ff70750bda52

--- Browser/gui_motif.c
+++ Browser/gui_motif.c
@@ -28,6 +28,8 @@ XtAppContext ctx;
 extern int argc;
 extern char** argv;
 
+void harp_info(Widget w, void* pointer, void* data) {}
+
 int harp_gui_init(void) {
 	XInitThreads();
 	top = XtVaAppInitialize(&ctx, "Harp", NULL, 0, &argc, argv, NULL, XtNtitle, "Harp", NULL);
@@ -41,6 +43,7 @@ int harp_gui_init(void) {
 	url = XmVaCreateText(form, "URL", XmNtopAttachment, XmATTACH_FORM, XmNleftAttachment, XmATTACH_FORM, XmNrightAttachment, XmATTACH_WIDGET, NULL);
 	XtManageChild(url);
 	logo_button = XmVaCreatePushButton(form, "Logo", XmNlabelType, XmPIXMAP, XmNlabelPixmap, harp, XmNrightAttachment, XmATTACH_FORM, XmNtopAttachment, XmATTACH_FORM, XmNheight, 64 + 24, XmNwidth, 64 + 24, NULL);
+	XtAddCallback(logo_button, XmNactivateCallback, harp_info, NULL);
 	XtManageChild(logo_button);
 	scrolled_window = XmVaCreateScrolledWindow(form, "ScrolledW", XmNtopAttachment, XmATTACH_WIDGET, XmNtopWidget, logo_button, XmNbottomAttachment, XmATTACH_FORM, XmNleftAttachment, XmATTACH_FORM, XmNrightAttachment, XmATTACH_FORM, XmNbackground, 0xffffff, NULL);
 	XtManageChild(scrolled_window);
