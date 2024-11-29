031020011392ba1ce3e2fa058f667e80455c6be4
Nishi <nishi@nishi.boats>
2024-11-29 02:16:26 +0000

    add url
    git-svn-id: file:///raid/svn-personal/harp/trunk@23 86f897ea-aefc-b749-bab6-ff70750bda52

--- Browser/gui_motif.c
+++ Browser/gui_motif.c
@@ -6,16 +6,21 @@
 #include <X11/Intrinsic.h>
 #include <X11/IntrinsicP.h>
 #include <Xm/Xm.h>
+#include <Xm/Text.h>
+#include <Xm/Label.h>
 #include <Xm/MainW.h>
 #include <Xm/Form.h>
+#include <Xm/PushB.h>
+#include <Xm/ScrolledW.h>
 
 #include "../Images/harp.xbm"
 
 Widget top;
 Widget main_window;
 Widget form;
-Widget url_label;
 Widget url;
+Widget logo_button;
+Widget scrolled_window;
 
 Pixmap harp;
 
@@ -33,7 +38,15 @@ int harp_gui_init(void) {
 	harp = XCreateBitmapFromData(XtDisplay(top), DefaultRootWindow(XtDisplay(top)), (char*)harp_bits, harp_width, harp_height);
 	main_window = XtVaCreateManagedWidget("MainWindow", xmMainWindowWidgetClass, top, NULL);
 	form = XmVaCreateForm(main_window, "Form", NULL);
+	url = XmVaCreateText(form, "URL", XmNtopAttachment, XmATTACH_FORM, XmNleftAttachment, XmATTACH_FORM, XmNrightAttachment, XmATTACH_WIDGET, NULL);
+	XtManageChild(url);
+	logo_button = XmVaCreatePushButton(form, "Logo", XmNlabelType, XmPIXMAP, XmNlabelPixmap, harp, XmNrightAttachment, XmATTACH_FORM, XmNtopAttachment, XmATTACH_FORM, XmNheight, 64 + 24, XmNwidth, 64 + 24, NULL);
+	XtManageChild(logo_button);
+	scrolled_window = XmVaCreateScrolledWindow(form, "ScrolledW", XmNtopAttachment, XmATTACH_WIDGET, XmNtopWidget, logo_button, XmNbottomAttachment, XmATTACH_FORM, XmNleftAttachment, XmATTACH_FORM, XmNrightAttachment, XmATTACH_FORM, XmNbackground, 0xffffff, NULL);
+	XtManageChild(scrolled_window);
 	XtManageChild(form);
+
+	XtVaSetValues(url, XmNrightWidget, logo_button, NULL);
 
 	XtResizeWidget(top, 800, 600, 1);
 	XtRealizeWidget(top);
