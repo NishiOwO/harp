3ce82c011982d90075a6828695f43d11862e4d00
Nishi <nishi@nishi.boats>
2024-11-30 08:51:21 +0000

    add
    git-svn-id: file:///raid/svn-personal/harp/trunk@40 86f897ea-aefc-b749-bab6-ff70750bda52

--- Contrib/IJG/jmemname.c
+++ Contrib/IJG/jmemname.c
@@ -22,6 +22,10 @@ extern void * malloc JPP((size_t size));
 extern void free JPP((void *ptr));
 #endif
 
+#ifdef __MINGW32__
+#include <io.h>
+#endif
+
 #ifndef SEEK_SET		/* pre-ANSI systems may not define this; */
 #define SEEK_SET  0		/* if not, assume 0 is correct */
 #endif
