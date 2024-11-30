082a64807133eb16a0a07dddb35f471e0755ceae
Nishi <nishi@nishi.boats>
2024-11-30 10:56:09 +0000

    fix
    git-svn-id: file:///raid/svn-personal/harp/trunk@49 86f897ea-aefc-b749-bab6-ff70750bda52

--- Platform/generic.mk
+++ Platform/generic.mk
@@ -11,6 +11,6 @@ YFLAGS = -y -d
 LEX = lex
 LFLAGS =
 CC = cc
-CFLAGS = -std=c99 -D_DEFAULT_SOURCE $(INCLUDES) $(OPTIMIZE) `pkg-config --cflags xft`
+CFLAGS = -std=c99 -D_DEFAULT_SOURCE $(INCLUDES) $(OPTIMIZE) `pkg-config --cflags x11` `pkg-config --cflags xt` `pkg-config --cflags xft`
 LDFLAGS = $(OPTIMIZE)
-LIBS = -lX11 -lXm -lXt -lXft -lpthread
+LIBS =`pkg-config --libs x11` -lXm `pkg-config --libs xt` `pkg-config --libs xft` -lpthread
