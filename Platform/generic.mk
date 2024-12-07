e7ec355ec4b1643f6a9a65e2d06fbbdee18caace
Nishi <nishi@nishi.boats>
2024-12-07 08:23:41 +0000

    fix libs
    git-svn-id: file:///raid/svn-personal/harp/trunk@83 86f897ea-aefc-b749-bab6-ff70750bda52

--- Platform/generic.mk
+++ Platform/generic.mk
@@ -11,4 +11,4 @@ LFLAGS =
 CC = cc
 CFLAGS = -std=c99 -D_DEFAULT_SOURCE $(INCLUDES) $(OPTIMIZE)
 LDFLAGS = $(OPTIMIZE) -L $(TOPDIR)/Contrib/GIF -L $(TOPDIR)/Contrib/JPEG -L $(TOPDIR)/Contrib/PNG -L $(TOPDIR)/Contrib/WWW -L $(TOPDIR)/Contrib/ZLib -L $(TOPDIR)/Contrib/cJSON
-LIBS = -lpthread -lcJSON -lpng -ljpeg -lgif -lz -lwww -lssl -lcrypto
+LIBS = -lpthread -lcJSON -lpng -ljpeg -lgif -lwww -lz -lssl -lcrypto
